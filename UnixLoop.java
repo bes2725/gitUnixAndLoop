bdee19b69709e0f25446b2840854bfe446d25ee6
Bailey Spicer <infectionlegion@gmail.com>
2016-03-21 15:21:39 -0400

    Pushes initial file to repository

---
+++ UnixLoop.java
@@ -0,0 +1,20 @@
+/*
+Bailey Spicer
+bes2725@email.vccs.edu
+March 21, 2016
+This is an authentication program. This program will request a username and password, and then
+when those are correct it will prompt for the user type until the correct on is entered.
+*/
+import javax.swing.JOptionPane;
+
+public class UnixLoop
+{
+	public static void main(String[] args)
+	{
+		final String USERNAME = Bailey, PASS = another0ne;
+
+
+
+
+	}
+}
